c760104b95bce9eb8bc38678a6b71a66e5348d69
Yilin Ma <yilinma@Yilins-MBP-2.lan>
2023-05-07 16:26:51 -0400

    Map class; Tested running game in character.py

--- gamedb/character.py
+++ gamedb/character.py
@@ -4,20 +4,20 @@ A character has a name, a character type, and characteristics.
 A character can move around a map, and perform actions.
 """
 
-from gamedb.map import Node
+import gamedb.map as map
 
 import gamedb.char_types as ctyp
 
 
 class Character():
-    def __init__(self, name: str, char_type: str, location: Node):
+    def __init__(self, name: str, char_type: str, location: map.Node):
         if not isinstance(name, str):
             raise TypeError(f'Bad type for name: {type(name)}')
         if not isinstance(char_type, str):
             raise TypeError(f'Bad type for char_type: {type(char_type)}')
         if not ctyp.exists(char_type):
             raise ValueError(f'Character type {char_type} does not exist.')
-        if not isinstance(location, Node):
+        if not isinstance(location, map.Node):
             raise TypeError(f'Bad type for location: {type(location)}')
         self.name = name
         self.char_type = char_type
@@ -48,33 +48,38 @@ class Character():
                 f'Bad type for ability_value: {type(detail_value)}')
         self.details[detail_name] = detail_value
 
-    def move(self, destination: Node):
-        if not isinstance(destination, Node):
+    def move(self, destination: map.Node):
+        if not isinstance(destination, map.Node):
             raise TypeError(f'Bad type for destination: {type(destination)}')
         self.location = destination
 
     def add_item(self, item):
-        pass
+        self.items.append(item)
 
 
 def main():
-    # Test Action
-    TEST_ACT = {'act_func': lambda x: x, 'act_obj': 'obj'}
+    # New character
+    character = Character('Bob', 'Warrior', map.dark_chamber)
+    print(f'You are {character.get_name()} the {character.get_char_type()}')
+    print(f'You are {character.get_location().get_descr()}')
 
-    # Test Node
-    courtyard = Node('Courtyard', {'test_act': TEST_ACT})
-    fountain = Node('Fountain', {'test_act': TEST_ACT})
+    # Start the game
+    print(f'Your choices are: {character.get_location().get_action_menu()}')
+    choice = int(input('What do you do? Enter 999 to quit.'))
 
-    # Test Character
-    ym = Character('ym', 'Wizard', courtyard)
-    print('name: ', ym.get_name())
-    print('type: ', ym.get_char_type())
-    print('details: ', ym.get_detail_dict())
+    while choice != 999:
+        location = character.get_location()
+        act_name = location.get_action_list()[choice]
+        print(f'You chose: {act_name}')
+        if location.perform_action(act_name, character) == map.SUCCESS:
+            print(f'Action {act_name} succeeded.')
+        else:
+            print(f'Action {act_name} failed.')
+        print(
+            f'Your choices are: {character.get_location().get_action_menu()}')
+        choice = int(input('What do you do? Enter 999 to quit.'))
 
-    # Test move
-    print('location: ', ym.get_location().get_descr())
-    ym.move(fountain)
-    print('location: ', ym.get_location().get_descr())
+    print('Goodbye!')
 
 
 if __name__ == '__main__':
--- gamedb/map.py
+++ gamedb/map.py
@@ -15,7 +15,7 @@ def add(character, item):
     """
     Add an item to a character.
     """
-    print(f'{item} added to {character})')
+    print(f'{item} added to {character.get_name()}')
     character.add_item(item)
     return SUCCESS
 
@@ -24,7 +24,7 @@ def move(character, node):
     """
     Moves a character to a node.
     """
-    print(f'{character} moved to {node})')
+    print(f'{character.get_name()} {node.get_descr()}')
     character.move(node)
     return SUCCESS
 
@@ -64,38 +64,82 @@ class Node():
 
 
 class Map():
-    def __init__(self):
-        pass
+    def __init__(self, graph: dict = {}):
+        if not isinstance(graph, dict):
+            raise TypeError(f'Bad type for graph: {type(graph)}')
+        for node, neighbor_list in graph.items():
+            if not isinstance(node, Node):
+                raise TypeError(f'Bad type for node: {type(node)}')
+            if not isinstance(neighbor_list, list):
+                raise TypeError(
+                    f'Bad type for neighbor_list: {type(neighbor_list)}')
+            for neighbor in neighbor_list:
+                if not isinstance(neighbor, Node):
+                    raise TypeError(f'Bad type for neighbor: {type(neighbor)}')
+        self.graph = graph
+
+    def add(self, src, dest):
+        if not isinstance(src, Node):
+            raise TypeError(f'Bad type for src: {type(src)}')
+        if not isinstance(dest, Node):
+            raise TypeError(f'Bad type for dest: {type(dest)}')
+        if src not in self.graph:
+            self.graph[src] = []
+        if dest not in self.graph[src]:
+            self.graph[src].append(dest)
+
+    def get_node_list(self):
+        return list(self.graph.keys())
+
+    def get_neighbors(self, node):
+        if node not in self.graph:
+            raise ValueError(f'No such node: {node}')
+        return self.graph[node]
 
 
 PICK_UP_SWORD = 'Pick up sword'
 GO_THROUGH_DOOR = 'Go through the door'
+TAME_HORSE = 'Tame the horse'
+KILL_HORSE = 'Kill the horse'
+
+GRASSY_FIELD_ACTIONS = {
+    TAME_HORSE: {
+        ACT_FUNC: add,
+        ACT_OBJ: 'horse',
+    },
+    KILL_HORSE: {
+        ACT_FUNC: add,
+        ACT_OBJ: 'meat',
+    },
+    # GO_THROUGH_DOOR: {
+    #     ACT_FUNC: move,
+    #     ACT_OBJ: dark_chamber,
+    # },
+}
+
+grassy_field = Node('on a grassy field. '
+                    + 'There is a friendly horse, and a door behind you.',
+                    GRASSY_FIELD_ACTIONS)
 
 
-other_location = Node('on a grassy field', {})
-
-
-ACTIONS = {
+DARK_CHAMBER_ACTIONS = {
     PICK_UP_SWORD: {
         ACT_FUNC: add,
         ACT_OBJ: 'sword',
     },
     GO_THROUGH_DOOR: {
         ACT_FUNC: move,
-        ACT_OBJ: other_location,
+        ACT_OBJ: grassy_field,
     },
 }
 
-
-location = Node('in a dark chamber. '
-                + 'There is a sword on the floor, and a door before you.',
-                ACTIONS)
+dark_chamber = Node('in a dark chamber. '
+                    + 'There is a sword on the floor, and a door before you.',
+                    DARK_CHAMBER_ACTIONS)
 
 
 def main():
-    print(f'You are {location.get_descr()}')
-    print(f'Your choices are: {location.get_action_menu()}')
-    # Here we will take input and execute an action.
+    pass
 
 
 if __name__ == '__main__':
